157b4b0efc0b56bea1ab5cb753eeb56ef963427c
Paris M <paris.mendoza@bosc.com.mx>
2018-08-15 17:25:50 -0500

    done debug_print

---
+++ functions.py
@@ -0,0 +1,14 @@
+def debug_print(data_name, data_to_print=None):
+    try:
+        bar = '=' * 20
+        data_name = bar + data_name + bar
+        if data_to_print is not None:
+            print(data_name)
+            print(type(data_to_print))
+            print(data_to_print)
+            print('=' * len(data_name))
+        else:
+            print(data_name)
+            print('-' * len(data_name))
+    except Exception as e:
+        debug_print('Oops, I was not expecting this', e)
